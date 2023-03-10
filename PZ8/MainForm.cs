097fbcb392c4409e901b3e17e2030fee45055830
deltaDelete <samansy@yandex.ru>
2023-03-10 09:28:02 +0700

    Добавьте файлы проекта.

---
+++ PZ8/MainForm.cs
@@ -0,0 +1,7 @@
+namespace PZ8;
+
+public partial class MainForm : Form {
+    public MainForm() {
+        InitializeComponent();
+    }
+}
---
+++ PZ8/Student.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PZ8;
+public class Student {
+    public string FIO { get; private set; }
+}
+
+class Session {
+    public ISessionType SessionType { get; private set; }
+
+    public string Subject { get; set; }
+
+    public void ChangeSessionType(ISessionType sessionType) {
+        SessionType = sessionType;
+    }
+}
+
+interface ISessionType {
+    public void Pass();
+}
+
+/// <summary>
+/// Зачет
+/// </summary>
+class Report : ISessionType {
+    public void Pass() {
+        Console.WriteLine("Зачет");
+    }
+}
+
+/// <summary>
+/// Диф зачет
+/// </summary>
+class DiffReport : ISessionType {
+    public void Pass() {
+        Console.WriteLine("Диф зачет");
+    }
+}
+
+/// <summary>
+/// Экзамен
+/// </summary>
+class Exam : ISessionType {
+    public void Pass() {
+        Console.WriteLine("Экзамен");
+    }
+}
